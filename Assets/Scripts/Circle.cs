112d334c91166cc10b8b8af73d8464811d4679e9
Zewodec <zewodec@gmail.com>
2022-11-11 21:16:11 +0200

    generate with corutine, delete objects, cicle edit
    Circle edit:
    - set random pos
    - set random color
    - ser random size

--- Assets/Scripts/Circle.cs
+++ Assets/Scripts/Circle.cs
@@ -2,16 +2,62 @@ using UnityEngine;
 
 public class Circle : MonoBehaviour
 {
+    [SerializeField] private Transform _transform;
     [SerializeField] private float _speed = 1f;
+    [SerializeField] private float _widthSpawnPostion = 9f;
+
+    [Header("Size of Circle")]
+    [Range(1f, 3f)]
+    [SerializeField] private float _maxSize = 3f;
+    [Range(0.1f, 1f)]
+    [SerializeField] private float _minSize = 0.25f;
+
+    private float _radius = 1f;
 
     private Rigidbody2D _rigidbody;
+    private SpriteRenderer _spriteRenderer;
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _transform = _rigidbody.transform;
+    }
+
+    private void Start()
+    {
+        setRandomColor();
+        setRandomSize();
+        setRandomPosition();
+    }
+
+    private void setRandomPosition()
+    {
+        Vector3 newPosition = _transform.position;
+        float safeSpawnZone = _widthSpawnPostion - (_radius / 2);
+        newPosition.x = Random.Range(-safeSpawnZone, safeSpawnZone);
+        _transform.position = newPosition;
+    }
+
+    private void setRandomSize()
+    {
+        _radius = Random.Range(_minSize, _maxSize);
+        Vector3 circleSize = new Vector3(_radius, _radius, 1);
+        _transform.localScale = circleSize;
+    }
+
+    private void setRandomColor()
+    {
+        Color newColor = new Color(Random.Range(0f,1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+        _spriteRenderer.color = newColor;
     }
 
     private void FixedUpdate()
+    {
+        MoveCircleDown();
+    }
+
+    private void MoveCircleDown()
     {
         Vector2 velocity = Vector2.down;
         _rigidbody.MovePosition(_rigidbody.position + velocity * _speed * Time.fixedDeltaTime);
--- Assets/Scripts/GenerateCircles.cs
+++ Assets/Scripts/GenerateCircles.cs
@@ -1,23 +1,29 @@
+using System.Collections;
 using UnityEngine;
 
 public class GenerateCircles : MonoBehaviour
 {
     [SerializeField] private GameObject _circle;
 
-    [SerializeField] private float _radius = 1f;
-    [SerializeField] private float _width = 9f;
+    //[SerializeField] private float _radius = 1f;
+    //[SerializeField] private static float _width = 9f;
     
-    private Vector3 _spawnPosition = new Vector3(0, 0, 0);
+    private Vector3 _spawnPosition = new Vector3();
 
     private void Start()
     {
-        InvokeRepeating("GenerateCircle", 1f, 3f);
+        _spawnPosition.y = gameObject.transform.position.y;
+        StartCoroutine(SpawnCircle());
     }
 
-    private void GenerateCircle()
+    IEnumerator SpawnCircle()
     {
-        float safeWidth = _width - (_radius / 2);
-        _spawnPosition.x = Random.Range(-safeWidth, safeWidth);
-        Instantiate(_circle, _spawnPosition, Quaternion.identity);
+        while (true)
+        {
+            //float safeWidth = _width - (_radius / 2);
+            //_spawnPosition.x = Random.Range(-safeWidth, safeWidth);
+            Instantiate(_circle, _spawnPosition, Quaternion.identity);
+            yield return new WaitForSeconds(3);
+        }
     }
 }
